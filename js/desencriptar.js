9e551263308e49847278857eb4c2ae6fdda44d43
Yurlian Sivira <yurlian.jsr@gmail.com>
2022-08-03 19:20:26 -0300

    Commit de aplicacion responsive con funcion de copiar agregada

--- js/desencriptar.js
+++ js/desencriptar.js
@@ -1,30 +1,51 @@
 let btnDesencriptar = document.querySelector("#desencriptar");
 let mensajeImagen = document.querySelector(".imagen-mensaje"); 
-let btnCopia = document.querySelector("#copiar");
+let btnCopia = document.getElementById("#copiar");
 
 btnDesencriptar.addEventListener("click", function(event){
     event.preventDefault();
-
+   
     let ingresado = document.getElementById("texto-ingresado");
     let textoIngresado2 = ingresado.value;
     textoIngresado2 = textoIngresado2.toLowerCase();
-
+    validar();
     desencriptar(textoIngresado2);
-    mensajeImagen.style.visibility = "hidden";
-    btnCopia.style.visibility = "visible";
-})
+    ingresado.value.innerHTML  = "";
+});
 
 function desencriptar(textoIngresado2){
     let textoDesencriptado;
     let decodificar = [["enter","e"],["imes","i"],["ai","a"],["ober","o"],["ufat","u"]]
    
-    for (let i=0; i< decodificar.length; i++){
-        if(textoIngresado2.includes(decodificar[i][0])){
-            textoIngresado2 = textoIngresado2.replaceAll(decodificar[i][0],decodificar[i][1])
-            textoDesencriptado = textoIngresado2;
+    if(isValido == true){
+        for (let i=0; i< decodificar.length; i++){
+            if(textoIngresado2.includes(decodificar[i][0])){
+                textoIngresado2 = textoIngresado2.replaceAll(decodificar[i][0],decodificar[i][1])
+                textoDesencriptado = textoIngresado2;
+            }
         }
-    }
+    mensajeImagen.style.visibility = "hidden";
     document.getElementById("procesado").innerHTML = textoDesencriptado;
+    document.getElementById("texto-ingresado").value = "";
+    document.getElementById("encriptar").style.visibility = "hidden";
+    }
+    
     return textoDesencriptado;
+}
+
+function copy(){
+    var content = document.getElementById("procesado").innerHTML;
+    if(!content == ""){
+        navigator.clipboard.writeText(content)
+            .then(() => {
+            alert("Texto copiado con exito");
+        });
+    mensajeImagen.style.visibility = "visible";    
+    }  
+    
+    document.getElementById("procesado").innerHTML = "";
+    document.getElementById("texto-ingresado").value = "";
+    document.getElementById("desencriptar").style.visibility = "visible";
+    document.getElementById("encriptar").style.visibility = "visible";
 }
 
--- js/encriptar.js
+++ js/encriptar.js
@@ -1,6 +1,6 @@
 let btnEncriptar = document.querySelector("#encriptar");
 let imagenMensaje = document.querySelector(".imagen-mensaje"); 
-
+let isValido = true;
 
 btnEncriptar.addEventListener("click", function(event){
     event.preventDefault();
@@ -8,26 +8,41 @@ btnEncriptar.addEventListener("click", function(event){
     let ingresado = document.getElementById("texto-ingresado");
     let textoIngresado = ingresado.value;
     textoIngresado = textoIngresado.toLowerCase();
-
+    validar();
     encriptar(textoIngresado);
-    imagenMensaje.style.visibility = "hidden";
-    btnCopiado.style.visibility = "visible";
-   
+    document.getElementById("texto-ingresado").value = "";   
+   console.log(textoIngresado);
 })
 
 function encriptar(textoIngresado){
     let textoEncriptado;  
     let texto = [ ["e", "enter"], ["i", "imes"],["a","ai"], ["o", "ober"], ["u", "ufat"]];
-        
-    for(let i = 0; i < texto.length; i++){
-        if(textoIngresado.includes(texto[i][0])){
-            textoIngresado = textoIngresado.replaceAll(texto[i][0], texto[i][1]);
-            textoEncriptado = textoIngresado;
-        }
-    }
-    document.getElementById("procesado").innerHTML = textoEncriptado;
+    if(isValido == true){
+
+       for(let i = 0; i < texto.length; i++){
+            if(textoIngresado.includes(texto[i][0])){
+                textoIngresado = textoIngresado.replaceAll(texto[i][0], texto[i][1]);
+                textoEncriptado = textoIngresado;
+            }
+        } 
+        mensajeImagen.style.visibility = "hidden";
+        document.getElementById("procesado").innerHTML = textoEncriptado;
+        document.getElementById("desencriptar").style.visibility = "hidden";
+    }    
     return textoEncriptado;
 }
 
+function validar(){
+    let entrada = document.getElementById("texto-ingresado").value;
 
+   if(entrada == ""){
+    alert("Debe ingresar un texto a encriptar o desencriptar");
+    isValido = false;
+    }else if(entrada.match(/[A-Z0-9áéíóú]/g)){
+    alert("Dato ingresado incorrecto...Favor ingresar solo letras minusculas, sin ascentos ni numeros");
+    isValido = false;
+   }else{
+        isValido = true;
+   }
+}
 
